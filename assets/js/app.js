b3815be6b82e7d7f589f579f49420160c23cf1f5
Harold DRF <95robots@gmail.com>
2019-02-07 23:33:05 -0700

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,30 @@
 //variables
-
+const tweetList = document.getElementById('tweet-list');
 
 //event listeners
+eventListeners();
+
 function eventListeners(){
-    document.querySelector('#form').addEventListener('submit',newMessage)
+    document.querySelector('#form').addEventListener('submit', newMessage);
 }
 
 
 //funtions
 function newMessage(e){
     e.preventDefault();
+
+    const tweet = document.getElementById('tweet').value;
+
+    const removeBtn = document.createElement('a');
+    removeBtn.classList = 'remove-tweet';
+    removeBtn.textContent = 'X';
+
+
+    const li = document.createElement('li');
+    li.textContent = tweet;
+    
+
+    li.appendChild(removeBtn);
+    tweetList.appendChild(li);
+
 }

655d7503fe42dfa6bd3140070577720964c2f3ca
Harold DRF <95robots@gmail.com>
2019-02-11 22:16:52 -0700

    Local storage working

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,10 +29,48 @@ function newMessage(e){
     li.appendChild(removeBtn);
     tweetList.appendChild(li);
 
+    addTweetLocalStorage(tweet);
 }
 
 function removeTweet(e){
     if(e.target.classList.contains('remove-tweet')){
         e.target.parentElement.remove();
     }
+}
+
+function addTweetLocalStorage(tweet){
+    let tweets = getTweetsLocalStorage();
+
+    tweets.push(tweet);
+
+    localStorage.setItem('tweets', JSON.stringify(tweets));
+}
+
+function getTweetsLocalStorage(tweet){
+    let tweets;
+    const tweetsLS = localStorage.getItem('tweets');
+    if(tweetsLS === null){
+        tweets=[];
+    }else{
+        tweets = JSON.parse(tweetsLS);
+    }
+    return tweets;
+}
+
+function localStorageOnLoad(){
+    let tweets = getTweetsLocalStorage();
+
+    tweets.forEach(tweet => {
+        const removeBtn = document.createElement('a');
+        removeBtn.classList = 'remove-tweet';
+        removeBtn.textContent = 'X';
+    
+    
+        const li = document.createElement('li');
+        li.textContent = tweet;
+        
+    
+        li.appendChild(removeBtn);
+        tweetList.appendChild(li);
+    });
 }
